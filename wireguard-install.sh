8b0b1a086b343030ca61b9527edd35d0f51f7009
Nyr <me@nyr.be>
2020-05-19 17:03:03 +0200

    Check for sbin in $PATH

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -60,6 +60,13 @@ This version of CentOS is too old and unsupported"
 	exit
 fi
 
+# Detect environments where $PATH does not include the sbin directories
+if ! grep -q sbin <<< $PATH; then
+	echo '$PATH does not include sbin
+Try using "su -" instead of "su"'
+	exit
+fi
+
 systemd-detect-virt -cq
 is_container="$?"
 
