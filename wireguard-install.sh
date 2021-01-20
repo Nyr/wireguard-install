61643bea19269de892d95ca50fe2d3e8b42a73bf
Brandon Ambrose <bpambrose97@gmail.com>
2021-01-20 08:58:13 -0500

    need to test, believe this is correct

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -100,7 +100,7 @@ new_client_dns () {
 	echo "   4) OpenDNS"
 	echo "   5) Quad9"
 	echo "   6) AdGuard"
-	echo "   7) WireGuard Interface"
+	echo "   7) Custom IP"
 	read -p "DNS server [1]: " dns
 	until [[ -z "$dns" || "$dns" =~ ^[1-7]$ ]]; do
 		echo "$dns: invalid selection."
@@ -135,7 +135,7 @@ new_client_dns () {
 			dns="94.140.14.14, 94.140.15.15"
 		;;
 		7)
-			dns="10.7.0.1"
+			read -p "Enter IP: " custom_dns_ip
 		;;
 	esac
 }
