429a409c72725bacfbd8f6f8898c7d4f49d57d93
Nyr <me@nyr.be>
2024-06-16 14:49:07 +0200

    Improve qrencode colors
    Forces black/white colors for the QR code.
    
    Useful in terminal emulators with custom color themes.

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -458,7 +458,7 @@ EOF
 		{ crontab -l 2>/dev/null; echo "$(( $RANDOM % 60 )) $(( $RANDOM % 3 + 3 )) * * * /usr/local/sbin/boringtun-upgrade &>/dev/null" ; } | crontab -
 	fi
 	echo
-	qrencode -t UTF8 < ~/"$client.conf"
+	qrencode -t ANSI256UTF8 < ~/"$client.conf"
 	echo -e '\xE2\x86\x91 That is a QR code containing the client configuration.'
 	echo
 	echo "Finished!"
@@ -497,7 +497,7 @@ else
 			# Append new client configuration to the WireGuard interface
 			wg addconf wg0 <(sed -n "/^# BEGIN_PEER $client/,/^# END_PEER $client/p" /etc/wireguard/wg0.conf)
 			echo
-			qrencode -t UTF8 < ~/"$client.conf"
+			qrencode -t ANSI256UTF8 < ~/"$client.conf"
 			echo -e '\xE2\x86\x91 That is a QR code containing your client configuration.'
 			echo
 			echo "$client added. Configuration available in:" ~/"$client.conf"
